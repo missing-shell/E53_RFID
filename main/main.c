34e4d9865bb8ec6d5f8e1b37d839b62b714bb347
shell <missing_shell@126.com>
2024-04-17 18:11:13 +0800

    Add read and write PICC code

--- main/main.c
+++ main/main.c
@@ -10,12 +10,24 @@
 #include "rc522.h"
 
 #define RC522_HOST SPI2_HOST
-#define PIN_NUM_SDA 21
-#define PIN_NUM_SCK 4
-#define PIN_NUM_MOSI 5
-#define PIN_NUM_MISO 22
+#define PIN_NUM_SDA (23)
+#define PIN_NUM_SCK (19)
+#define PIN_NUM_MOSI (18)
+#define PIN_NUM_MISO (20)
+#define PIN_NUM_RST (22)
 
-static const char TAG[] = "main";
+static const char TAG[] = "RC522_EXAMPLE";
+
+static void setup_3v3_output(uint8_t gpio_num)
+{
+    gpio_config_t ioconfig = {
+        .pin_bit_mask = (1ULL << gpio_num),
+        .mode = GPIO_MODE_OUTPUT};
+    gpio_config(&ioconfig);
+
+    // 设置GPIO为高电平
+    gpio_set_level(gpio_num, 1);
+}
 
 static rc522_handle_t scanner;
 /**
@@ -42,9 +54,20 @@ static void rc522_handler(void *arg, esp_event_base_t base, int32_t event_id, vo
         rc522_tag_t *tag = (rc522_tag_t *)data->ptr;
         // 打印标签的序列号信息
         ESP_LOGI(TAG, "Tag scanned (sn: %" PRIu64 ")", tag->serial_number);
+        // 打印标签的序列号信息为十六进制
+        ESP_LOGI(TAG, "Tag scanned (sn: %02llx)", tag->serial_number);
+    }
+    break;
+
+        // 处理未知事件
+    default:
+    {
+        ESP_LOGW(TAG, "Unknown event id: %ld", event_id);
     }
     break;
     }
+
+    ESP_LOGI(TAG, "----------------------------");
 }
 /**
  * 初始化RC522 RFID模块。
@@ -55,11 +78,13 @@ static void rc522_handler(void *arg, esp_event_base_t base, int32_t event_id, vo
 static esp_err_t rc522_initialize()
 {
     rc522_config_t config = {
+
         .spi.host = RC522_HOST,
         .spi.miso_gpio = PIN_NUM_MISO,
         .spi.mosi_gpio = PIN_NUM_MOSI,
         .spi.sck_gpio = PIN_NUM_SCK,
         .spi.sda_gpio = PIN_NUM_SDA,
+
     };
 
     // 调用rc522_create并处理错误
@@ -79,12 +104,15 @@ static esp_err_t rc522_initialize()
 
     // 启动扫描器
     rc522_start(scanner);
-
+    rc522_enable_write_mode(scanner, 2);
     return ESP_OK;
 }
 
 void app_main()
 {
+
+    // 设置GPIO3为3.3V输出-RST引脚
+    setup_3v3_output(PIN_NUM_RST);
 
     esp_err_t ret = rc522_initialize();
     if (ret != ESP_OK)
@@ -93,10 +121,6 @@ void app_main()
         ESP_LOGE(TAG, "Failed to init rc522");
     }
 
-    while (1)
-    {
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
     // 在程序结束或不需要使用RC522时，确保正确释放资源
-    rc522_destroy(scanner);
+    // rc522_destroy(scanner);
 }
